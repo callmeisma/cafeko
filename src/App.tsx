7846fb17bdce4c3c4e4d750d2e0bc814640ae2b1
callmeisma <ismael.fdez87@gmail.com>
2026-08-20 01:23:31 -0400

    menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
+import Menu from "./pages/Menu";
 
 export default function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
